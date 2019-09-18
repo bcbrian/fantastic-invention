87fa727f2a296e3a223d3bf12e74ff25b03459b6
Brian Bartholomew <me@bcbrian.com>
2019-09-17 21:22:12 -0500

    got the fundamentals working

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-function sayHiYo() {
-  alert("hi yo!");
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+function getRandomValue(arr) {
+  const valueIndex = Math.floor(Math.random() * arr.length);
+  const value = arr[valueIndex];
+  return value;
 }
+
+function startGame() {
+  const choices = [];
+
+  while (choices.length < 3) {
+    const randLetter = getRandomValue(letters);
+    if (choices.includes(randLetter) === false) {
+      choices.push(randLetter);
+    }
+  }
+
+  const letter = getRandomValue(choices);
+
+  alert("what matches " + letter + " in " + choices);
+}
+
+startGame();
