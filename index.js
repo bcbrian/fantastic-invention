3f820ac0573f2b2b35184e9e9e1174b8e32fb461
Brian Bartholomew <me@bcbrian.com>
2019-09-18 20:50:53 -0500

    got the game working in the ui

--- index.js
+++ index.js
@@ -38,14 +38,33 @@ function startGame() {
 
   while (choices.length < 3) {
     const randLetter = getRandomValue(letters);
-    if (choices.includes(randLetter) === false) {
+    if (!choices.includes(randLetter)) {
       choices.push(randLetter);
     }
   }
 
   const letter = getRandomValue(choices);
+  const redZaneEl = document.getElementById("RedZane");
+  redZaneEl.innerHTML = letter.toUpperCase();
 
-  alert("what matches " + letter + " in " + choices);
+  const wolfhoundjesseEl = document.getElementById("wolfhoundjesse");
+  wolfhoundjesseEl.innerHTML = "";
+  // ["a", "b", "c"]
+  for (let i = 0; i < choices.length; i++) {
+    const choice = choices[i];
+    const btnEl = document.createElement("button");
+    btnEl.innerHTML = choice;
+    btnEl.classList.add("btn-choice");
+    wolfhoundjesseEl.append(btnEl);
+
+    btnEl.addEventListener("click", function() {
+      if (choice.toLowerCase() === letter.toLowerCase()) {
+        alert("winner");
+        startGame();
+      }
+    });
+  }
+  // alert("what matches " + letter + " in " + choices);
 }
 
 startGame();
